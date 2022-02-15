0b3987afb5b52a020d21d8957519c659bf87e915
irfan dayan <engr.irfandayan@gmail.com>
2022-02-15 19:26:26 +0500

    Add Header & Home components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
 import Login from "./components/Login";
 
 const App = () => {
@@ -7,6 +10,7 @@ const App = () => {
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={[<Header />, <Home />]} />
       </Routes>
     </div>
   );
