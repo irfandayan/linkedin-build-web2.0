3e2dc662301c6e53c68a0737d6448fdbc2d2fc6b
irfan dayan <engr.irfandayan@gmail.com>
2022-02-16 14:36:55 +0500

    Add Home, Leftside, Main & Rightside

---
+++ src/components/RightSide.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const RightSide = (props) => {
+  return <Container>RightSide</Container>;
+};
+
+const Container = styled.div`
+  grid-area: reftside;
+`;
+
+export default RightSide;
